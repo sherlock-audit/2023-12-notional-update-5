00b6e31186af85b0829c2906bfc6442735dd3741
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-01-05 17:23:51 +0100

    Delete scope directories

--- wrapped-fcash/interfaces/notional/IWrappedfCashFactory.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity >=0.7.6;
-
-interface IWrappedfCashFactory {
-    event WrapperDeployed(uint16 currencyId, uint40 maturity, address wrapper);
-
-    function deployWrapper(uint16 currencyId, uint40 maturity) external returns (address);
-    function computeAddress(uint16 currencyId, uint40 maturity) external view returns (address);
-}
