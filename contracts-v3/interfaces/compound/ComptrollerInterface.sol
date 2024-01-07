5b4b122fdba4c981f3797a32843d3f501b0078ae
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-01-07 18:27:26 +0100

    Delete scope directories

--- contracts-v3/interfaces/compound/ComptrollerInterface.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BSUL-1.1
-pragma solidity >=0.7.6;
-
-interface Comptroller {
-    function claimComp(address holder, address[] calldata ctokens) external;
-
-    function getCompAddress() external view returns (address);
-}
